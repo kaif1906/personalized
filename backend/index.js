43b3c0a32c32b788af79367fcf06cd85a2e3245f
kaif1906 <siddiquekaif38@gmail.com>
2025-10-12 20:31:44 +0530

    final

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // --- Middleware ---
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "uploads"))); // serve images
 
